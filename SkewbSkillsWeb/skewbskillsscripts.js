1a9756f2f0aed00d9fac9aac60d14604afd03fbc
AnnikaStein <annika-stein@t-online.de>
2019-09-13 18:23:30 +0200

    added donate button

--- SkewbSkillsWeb/skewbskillsscripts.js
+++ SkewbSkillsWeb/skewbskillsscripts.js
@@ -1,16 +1,23 @@
 function toggleFLT() {
   var x = document.getElementById("FLT");
+  var y = document.getElementById("AlgT");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
   } else {
     x.style.display = "none";
   }
 }
 function toggleAlgT() {
   var x = document.getElementById("AlgT");
+  var y = document.getElementById("FLT");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
   } else {
     x.style.display = "none";
   }
+}
+function changescrlen() {
+
 }
