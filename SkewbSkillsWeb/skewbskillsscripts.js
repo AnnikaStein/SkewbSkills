3e131bae208c2b1516813c1b5fe0c914e34b83b7
AnnikaStein <annika-stein@t-online.de>
2019-09-13 20:51:40 +0200

    added donate button

--- SkewbSkillsWeb/skewbskillsscripts.js
+++ SkewbSkillsWeb/skewbskillsscripts.js
@@ -18,6 +18,79 @@ function toggleAlgT() {
     x.style.display = "none";
   }
 }
+function toggleSelectAlgs() {
+  var x = document.getElementById("selectAlgs");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function whattodowitht() {
+     if (document.getElementById("l4c").checked == false
+     || document.getElementById("l5c").checked == false
+     || document.getElementById("pi3s").checked == false
+     || document.getElementById("pihu").checked == false
+     || document.getElementById("pihz").checked == false
+     || document.getElementById("pio").checked == false
+     || document.getElementById("piswirl").checked == false
+     || document.getElementById("pivu").checked == false
+     || document.getElementById("piwat").checked == false
+     || document.getElementById("pix").checked == false
+     || document.getElementById("pizconj").checked == false
+     || document.getElementById("p3s").checked == false
+     || document.getElementById("phu").checked == false
+     || document.getElementById("phzpure").checked == false
+     || document.getElementById("po").checked == false
+     || document.getElementById("pswirl").checked == false
+     || document.getElementById("pvu").checked == false
+     || document.getElementById("pwat").checked == false
+     || document.getElementById("px").checked == false
+     || document.getElementById("pzconj").checked == false) {
+        document.getElementById("l4c").checked = true;
+        document.getElementById("l5c").checked = true;
+        document.getElementById("pi3s").checked = true;
+        document.getElementById("pihu").checked = true;
+        document.getElementById("pihz").checked = true;
+        document.getElementById("pio").checked = true;
+        document.getElementById("piswirl").checked = true;
+        document.getElementById("pivu").checked = true;
+        document.getElementById("piwat").checked = true;
+        document.getElementById("pix").checked = true;
+        document.getElementById("pizconj").checked = true;
+        document.getElementById("p3s").checked = true;
+        document.getElementById("phu").checked = true;
+        document.getElementById("phzpure").checked = true;
+        document.getElementById("po").checked = true;
+        document.getElementById("pswirl").checked = true;
+        document.getElementById("pvu").checked = true;
+        document.getElementById("pwat").checked = true;
+        document.getElementById("px").checked = true;
+        document.getElementById("pzconj").checked = true;
+     } else {
+        document.getElementById("l4c").checked = false;
+        document.getElementById("l5c").checked = false;
+        document.getElementById("pi3s").checked = false;
+        document.getElementById("pihu").checked = false;
+        document.getElementById("pihz").checked = false;
+        document.getElementById("pio").checked = false;
+        document.getElementById("piswirl").checked = false;
+        document.getElementById("pivu").checked = false;
+        document.getElementById("piwat").checked = false;
+        document.getElementById("pix").checked = false;
+        document.getElementById("pizconj").checked = false;
+        document.getElementById("p3s").checked = false;
+        document.getElementById("phu").checked = false;
+        document.getElementById("phzpure").checked = false;
+        document.getElementById("po").checked = false;
+        document.getElementById("pswirl").checked = false;
+        document.getElementById("pvu").checked = false;
+        document.getElementById("pwat").checked = false;
+        document.getElementById("px").checked = false;
+        document.getElementById("pzconj").checked = false;
+     }
+
+}
 function changescrlen() {
 
 }
