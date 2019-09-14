39018a2e21d9476e43ab4827590d744b35829efa
AnnikaStein <annika-stein@t-online.de>
2019-09-14 21:36:49 +0200

    added donate button

--- SkewbSkillsWeb/skewbskillsscripts.js
+++ SkewbSkillsWeb/skewbskillsscripts.js
@@ -1,3 +1,6 @@
+ReadAllScrambles()
+ReadAllScramblesAlg()
+
 function toggleFLT() {
   var x = document.getElementById("FLT");
   var y = document.getElementById("AlgT");
@@ -26,6 +29,7 @@ function toggleSelectAlgs() {
     x.style.display = "none";
   }
 }
+
 function whattodowitht() {
      if (document.getElementById("l4c").checked == false
      || document.getElementById("l5c").checked == false
@@ -91,6 +95,246 @@ function whattodowitht() {
      }
 
 }
+
+function ReadAllScrambles() {
+    var posschars = ["R", "R\'", "L", "L\'", "U", "U\'", "B", "B\'"];
+    // onemovers
+    var newscramble1list = [];
+    for (var i = 0; i < posschars.length; i++) {
+        newscramble1list.push(posschars[i]);
+    }
+    var scramblelist = newscramble1list;
+
+    //twomovers
+    var newscramble2list = [];
+    for (j = 0; j < newscramble1list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (posschars[i].indexOf(newscramble1list[j]) != -1) {
+                continue;
+            } else if (newscramble1list[j].indexOf(posschars[i]) != -1) {
+                continue;
+            } else {
+                newscramble2list.push(newscramble1list[j]+" "+posschars[i]);
+            }
+        }
+    }
+    //threemovers
+    var newscramble3list = [];
+    for (j = 0; j < newscramble2list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (newscramble2list[j][-2] == " ") {
+                if (newscramble2list[j][-1] == posschars[i][0]) {
+                    continue;
+                }
+            } else {
+                if (newscramble2list[j][-2] == posschars[i][0]) {
+                    continue;
+                }
+            }
+            newscramble3list.push(newscramble2list[j]+" "+posschars[i]);
+        }
+    }
+    //fourmovers
+    var newscramble4list = [];
+    for (j = 0; j < newscramble3list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (newscramble3list[j][-2] == " ") {
+                if (newscramble3list[j][-1] == posschars[i][0]) {
+                    continue;
+                }
+            } else {
+                if (newscramble3list[j][-2] == posschars[i][0]) {
+                    continue;
+                }
+            }
+            newscramble4list.push(newscramble3list[j]+" "+posschars[i]);
+        }
+    }
+    //fivemovers
+    var newscramble5list = [];
+    for (j = 0; j < newscramble4list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (newscramble4list[j][-2] == " ") {
+                if (newscramble4list[j][-1] == posschars[i][0]) {
+                    continue;
+                }
+            } else {
+                if (newscramble4list[j][-2] == posschars[i][0]) {
+                    continue;
+                }
+            }
+            newscramble5list.push(newscramble4list[j]+" "+posschars[i]);
+        }
+    }
+    //sixmovers
+    var newscramble6list = [];
+    for (j = 0; j < newscramble5list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (newscramble5list[j][-2] == " ") {
+                if (newscramble5list[j][-1] == posschars[i][0]) {
+                    continue;
+                }
+            } else {
+                if (newscramble5list[j][-2] == posschars[i][0]) {
+                    continue;
+                }
+            }
+            newscramble6list.push(newscramble5list[j]+" "+posschars[i]);
+        }
+    }
+    //sevenmovers
+    var newscramble7list = [];
+    for (j = 0; j < newscramble6list.length; j++) {
+        for (i = 0; i < posschars.length; i++) {
+            if (newscramble6list[j][-2] == " ") {
+                if (newscramble6list[j][-1] == posschars[i][0]) {
+                    continue;
+                }
+            } else {
+                if (newscramble6list[j][-2] == posschars[i][0]) {
+                    continue;
+                }
+            }
+            newscramble7list.push(newscramble6list[j]+" "+posschars[i]);
+        }
+    }
+}
+function ReadAllScramblesAlg() {
+    var scrpiswirl = ["r' R' r R' r z' r z r R'",
+            "r R r R' z R r' R' r z2 R' r' R' r",
+            "R r' b' r' R r' R r",
+            "r' B R r R' z R b' R'",
+            "R' B R' B' r' R r z R r R'",
+            "R r' R r z R r' R' z' r' R' r",
+            "r' R r R b' r R' b'",
+            "R r R' z R B r' R' r'"];
+    var scrpiwat = ["B' R' r R' B' r' R' r'",
+            "R' r' R r R B R' B' R' B R B'",
+            "r R' r B r' z r R' r' R' r",
+            "R r' R' r' R B' r z R r' R",
+            "R' r B' r' R r' z R r' R r",
+            "r' R' r B' R' z R r' R' r R r'",
+            "R' r R' r' B R B' r R r'",
+            "r' R r R r' R' r B R B' R"];
+    var scrpix = ["R' r R' r' z' r' R' r z R' r",
+            "R r' R r z2 R r' R' r b",
+            "R r R' r z2 r' R r z r R r",
+            "B R r' R' r z2 R r R' r",
+            "b R r' R' r z y l r' R r",
+            "B' r' R r z r' R' z R r' R'",
+            "R r R' r z2 R r' R' r B",
+            "r' R' r z r' R' B R B' r'"];
+    var scrpihu = ["r B' r' R r R' r f'",
+            "R' b R r' R' r R' b",
+            "B r' R r' R' r B r'",
+            "l' B b' r B r' R' r",
+            "r R' r b' B' R r' R",
+            "r' R r' B R B' r' R",
+            "R' r R' B b r' R r'",
+            "r' B R B' z' r' R r B'"];
+    var scrpivu = ["R r' R r B' R' B r' R' r",
+            "B' r' R r z B R B' r'",
+            "R B R r' R' B'",
+            "b' r' R' r y r B"];
+    var scrpio = ["r' R' r z R r' R' r R r R'",
+            "R r R' z' r' R r R' r' R' r",
+            "R r' R' r' R r R' z' r' R r",
+            "r' R r R r' R' r B R' B'",
+            "B' R' r B' r' R r'",
+            "r R B' r z R r' R",
+            "r R' r B r' R B",
+            "R' r R' z' r' B R' r'"];
+    var scrpizconj = ["B' r' R' r B r' R r",
+            "r B R B' r' B R' B'",
+            "B R B' r B R' B' r'",
+            "r' R' r B' r' R r B",
+            "r' R r R' z R r' R' r z' R r' R' r",
+            "r' R r R' z r' R r R' z' R r' R' r",
+            "R r' R' r z' r' R r R' z r' R r R'",
+            "R r' R' r z' R r' R' r z r' R r R'"];
+    var scrpi3s = ["r R' r' z' r' R' r z r R' r' R' r",
+            "r' R r R r' z' r' R r z r R r'"];
+    var scrpihz = ["R' r' R' r' B R' B' r'",
+            "r' R' r z r R r' R' z' r' R r",
+            "R r R' z' r' R' r R z R r' R'"];
+    var scrpswirl = ["r R r R' z R r' R' r b",
+            "b' r' R r R' z' R r' R' r'",
+            "R r' R r B' r' R r B",
+            "R' r' R' r z R r R' r",
+            "r' R' r R' z' R r' R' r B'",
+            "R r' R r R' z' R r' R' r B'",
+            "r' R r' R' z' r' R r R",
+            "R r' R r z R r' R' r'"];
+    var scrpwat = ["r R r R' z R r' R' r' z' r R'",
+            "r' R r' z' r' R' r z r' R r R",
+            "R r B R B' r' R",
+            "B R B' r' R r R",
+            "r R' B' l r' y r R r'",
+            "R' r R z B' b r' R' r",
+            "r' R r' R' r' z' r' R' r R",
+            "R r' R r R z R r R' r'"];
+    var scrpx = ["R r' R' r z' R r' R' r",
+            "r' B r' R r R' r' B",
+            "r' R r R' z r' R r R'",
+            "B' r R r' R' r B' r",
+            "r' R' r z' r' R r R' B",
+            "B' R r' R' r z r' R r",
+            "B' r' R r R' z R r' R' r R",
+            "b r' R r R' z' R r' R'"];
+    var scrphu = ["r' l r R r' l' r R'",
+            "r R' B R r' y' r' R' r",
+            "r R' r' B' r R r' B",
+            "B' r R' r' B r R r'",
+            "R r' z' r' R r z r R r' R",
+            "R r R' r' R' z' r' R' r z R r",
+            "R r' R' z R r y r R' B R",
+            "r R' r z r R r R' r' z' r"];
+    var scrpvu = ["r' R r R' z' R r R' r'",
+            "R r' R' r z r' R' r R",
+            "r R' r' z' r' R' r z r R",
+            "R' r R z R r R' z' R' r'"];
+    var scrpo = ["r' R' r R z R r R' r'",
+            "R r R' r' z' r' R' r R",
+            "r R r' R' z' R' r' R r",
+            "R' r' R r z r R r' R'",
+            "r R B R B' R' r' R'",
+            "R r R B R' B' R' r'",
+            "R' r' R' r l r' R r",
+            "r' R' r z r' R' r R r"];
+    var scrpzconj = ["r' R' r z r' R r' R' r R r' R'",
+            "R r R' z' R r' R r R' r' R r",
+            "R r R' r' R r R' z' R r' R r",
+            "r' R' r R r' R' r z r' R r' R'",
+            "R r' R' r z R r' R' r z' R r' R' r",
+            "R r' R' r z' R r' R' r z R r' R' r",
+            "r' R r R' z' r' R r R' z r' R r R'",
+            "r' R r R' z r' R r R' z' r' R r R'"];
+    var scrp3s = ["r' R r R' r' R r R' z R r' R' r",
+            "R r' R' r z' R r' R' r R r' R' r",
+            "r' R r R' r' R r R' z r' R r R'",
+            "r' R r R' z r' R r R' r' R r R'"];
+    var scrphzpure = ["r' R r R' z B R r' R' r B'",
+            "R r' R' r z r' R' r x R r R'",
+            "r R r' R' z' r' R' r z r' R r' R'",
+            "B L' r B' l' r' y r' R' r"];
+    var scrl4c = ["r' l r' l' B' l' B l r'",
+            "B' r R' B x R r' R r'",
+            "R' r' R' r z2 R r' R' r b'",
+            "r' R r' R' r z2 R r' R' r B"];
+    var scrl5c = ["R' b R r' R' r R' b R r' R' r",
+            "r' R B R' r B R' z b' r R'",
+            "R' r B' r' R r R' r B' r' R r",
+            "r B' r' R r R' r z2 r' R r' R' r",
+            "R' r R B' r B R' B r' R r",
+            "r' B R r R' B r' z r' R' r"];
+
+    var scramblelistAlg = ["R R'"];
+}
+// bis hier hin alles ok
+
+
+// testen ab hier
+/*
 function decrMoves() {
 
 }
@@ -109,3 +353,29 @@ function ScramblePlusColour() {
 function ScramblePlusColourAlg() {
 
 }
+*/
+function threeswap(listname,i,j,k) {
+    [listname[j], listname[k]] = [listname[k], listname[j]];
+    [listname[i], listname[j]] = [listname[j], listname[i]];
+}
+function fourswap(listname,i,j,k,l) {
+    [listname[k], listname[l]] = [listname[l], listname[k]];
+    [listname[j], listname[k]] = [listname[k], listname[j]];
+    [listname[i], listname[j]] = [listname[j], listname[i]];
+}
+/* this function uses Qt and should be replaced
+function corrcol(list,i) {
+    if (list[i] == "o") {
+        return //QtGui.QColor( 0xFF, 0x68, 0x00 )
+    } else if (list[i] == "g") {
+        return //Qt.green
+    } else if (list[i] == "r") {
+        return //Qt.red
+    } else if (list[i] == "b") {
+        return //Qt.blue
+    } else if (list[i] == "w") {
+        return //Qt.white
+    } else {
+        return //Qt.yellow
+    }
+}*/
